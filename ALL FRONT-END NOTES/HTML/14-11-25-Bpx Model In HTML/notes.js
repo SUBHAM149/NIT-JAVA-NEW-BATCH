03c425181e652f75fae4c1244bf54dfb9cfae8ee
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-01-23 16:59:12 +0530

    Add files via upload

---
+++ ALL FRONT-END NOTES/HTML/14-11-25-Bpx Model In HTML/notes.js
@@ -0,0 +1,21 @@
+/*
+
+cellspacing : space outside the border : margin
+cellpadding : space inside the border  : padding 
+border : a line b/w margin & padding
+
+content -> padding -> border -> margin
+
+
+margin : it is a space outside the border 
+padding : it is a space inside the border
+border : a line b/w the margin and padding 
+content : any form of data 
+
+how to use or write the values of margin , padding & border
+
+border == margin == padding
+
+
+
+*/
