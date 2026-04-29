cd2b5ffcbc272ccb2caaa39a2437b4b5f482b0c7
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-04-29 22:47:52 +0530

    Add Course entity with ManyToMany relationship

---
+++ All ADV JAVA NOTES/Day-42-29-04-26-HibernetProject_10-Bidirectional-Many to many/Course.java
@@ -0,0 +1,50 @@
+package com.nit.dto;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int c_id;
+    private String c_name;
+
+    @ManyToMany(mappedBy = "course")
+    private List<Student> student;
+
+    public int getC_id() {
+        return c_id;
+    }
+
+    public void setC_id(int c_id) {
+        this.c_id = c_id;
+    }
+
+    public String getC_name() {
+        return c_name;
+    }
+
+    public void setC_name(String c_name) {
+        this.c_name = c_name;
+    }
+
+    public List<Student> getStudent() {
+        return student;
+    }
+
+    public void setStudent(List<Student> student) {
+        this.student = student;
+    }
+
+    @Override
+    public String toString() {
+        return "Course [c_id=" + c_id + ", c_name=" + c_name + "]";
+    }
+}
