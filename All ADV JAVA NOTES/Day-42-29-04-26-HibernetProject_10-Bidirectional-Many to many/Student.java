6de0cc9cdc2c71160811de5cfd1d0edc3c728a76
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-04-29 22:48:20 +0530

    Create Student.java

---
+++ All ADV JAVA NOTES/Day-42-29-04-26-HibernetProject_10-Bidirectional-Many to many/Student.java
@@ -0,0 +1,58 @@
+package com.nit.dto;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.CascadeType;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int s_id; 
+    private String s_name;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+        name = "student_course",
+        joinColumns = @JoinColumn(name = "student_id"),
+        inverseJoinColumns = @JoinColumn(name = "course_id")
+    )
+    private List<Course> course;
+
+    public int getS_id() {
+        return s_id;
+    }
+
+    public void setS_id(int s_id) {
+        this.s_id = s_id;
+    }
+
+    public String getS_name() {
+        return s_name;
+    }
+
+    public void setS_name(String s_name) {
+        this.s_name = s_name;
+    }
+
+    public List<Course> getCourse() {
+        return course;
+    }
+
+    public void setCourse(List<Course> course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [s_id=" + s_id + ", s_name=" + s_name + "]";
+    }
+}
