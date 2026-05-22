933a4350da7c8d816114d334574064d60b1bc6b3
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-05-22 14:21:36 +0530

    Create DataPrinter.java

---
+++ All SPRING BOOT NOTES/Day-08-22-05-26-SpringBoot_Current DT,wish message,DiscountCalculator,ElectricityBillGenerator-9,10,11,12,13/SpringBoot_CurrentDate_Setter_Project_10/com.nit.beans/DataPrinter.java
@@ -0,0 +1,22 @@
+package com.nit.beans;
+
+import java.time.LocalDate;
+
+public class DatePrinter {
+         private LocalDate currentDate;
+         
+         
+
+		 public void setCurrentDate(LocalDate currentDate) {
+			 this.currentDate = currentDate;
+		 }
+         
+         
+         
+         public void printDate()
+         {
+        	 System.out.println("Current Date is: "+currentDate);
+         }
+         
+         
+}
