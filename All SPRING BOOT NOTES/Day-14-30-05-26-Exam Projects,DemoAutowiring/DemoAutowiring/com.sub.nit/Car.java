95905ab45277e8521ce2e90a6f2e39f446bef951
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-06-01 22:13:18 +0530

    Create Car.java

---
+++ All SPRING BOOT NOTES/Day-14-30-05-26-Exam Projects,DemoAutowiring/DemoAutowiring/com.sub.nit/Car.java
@@ -0,0 +1,11 @@
+package com.sub.nit;
+
+public class Car {
+	  Engine e;
+	  public Car(Engine e2) {
+		  this.e=e2;
+	  }
+      public void drive() {
+    	  System.out.println(" We are driving car");
+      }
+}
