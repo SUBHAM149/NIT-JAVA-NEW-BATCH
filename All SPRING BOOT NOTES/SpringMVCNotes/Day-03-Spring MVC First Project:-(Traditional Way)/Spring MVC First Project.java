0ca7c4685af184eafe8bafd4e0dd4f51fd026cca
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-06-28 22:48:38 +0530

    Add notes for Spring MVC first project setup

---
+++ All SPRING BOOT NOTES/SpringMVCNotes/Day-03-Spring MVC First Project:-(Traditional Way)/Spring MVC First Project.java
@@ -0,0 +1,18 @@
+15/06/2026
+----------
+1.Create a Dynamic Project
+2.Convert it into Maven Project
+3.Add the dependency Spring mvc and servlet dependency
+4.update project and build path is optional
+5.web.xml--->servlet mapping,and servlet class tag(Basically location of Dispatcher
+Servlet)
+6.spring-servlet.xml-->(Specifying base package and view resolver) inside WEB_INF Folder
+7.views inside WEB-INF
+8.create a html or jsp file inside webapp
+9.and specify a url
+10.for handling that url Create a controller class and provide a method
+which will handle the URL.
+
+
+Note:-Whenever we will add spring mvc dependency dispather servlet will be
+added by default.
