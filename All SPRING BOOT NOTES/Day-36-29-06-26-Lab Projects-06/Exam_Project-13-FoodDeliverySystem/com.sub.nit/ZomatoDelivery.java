039eb7b88d76224f7990d3fe6ef96c2acae7a764
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-04 22:28:31 +0530

    Create ZomatoDelivery.java

---
+++ All SPRING BOOT NOTES/Day-36-29-06-26-Lab Projects-06/Exam_Project-13-FoodDeliverySystem/com.sub.nit/ZomatoDelivery.java
@@ -0,0 +1,13 @@
+package com.sub.nit;
+
+public class ZomatoDelivery implements DeliveryService {
+
+    @Override
+    public void deliverOrder() {
+
+        System.out.println("Order is delivered using Zomato Delivery.");
+
+    }
+
+}
+
