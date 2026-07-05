fdd43c66703351f9292cec11f00600ba08250551
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-05 22:21:18 +0530

    Create Student class with fields and methods
    Added Student class with properties and getters/setters.

---
+++ All SPRING BOOT NOTES/Day-36-29-06-26-Lab Projects-06/Exam_Project-15-StudentRegistrationSystem/com.sub.model/Student.java
@@ -0,0 +1,31 @@
+package com.sub.model;
+public class Student {
+
+    private int studentId;
+    private String studentName;
+    private String department;
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
