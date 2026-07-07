805452ae5f8a3d28e6da32d7f64af8d7fb74d07e
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-07 11:52:26 +0530

    Add FoodCart class for managing dishes

---
+++ ALL SPRING BOOT NOTES PRASANT SIRS/SPRING CORE/ALL LAB PROJECTS/LabProject-01-SpringCoreSetterInjection/com.sub.program06/FoodCart.java
@@ -0,0 +1,25 @@
+package com.sub.program06;
+
+import java.util.List;
+
+public class FoodCart {
+
+    private List<String> dishes;
+
+    public FoodCart() {
+        super();
+    }
+
+    public List<String> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(List<String> dishes) {
+        this.dishes = dishes;
+    }
+
+    @Override
+    public String toString() {
+        return "FoodCart [dishes=" + dishes + "]";
+    }
+}
