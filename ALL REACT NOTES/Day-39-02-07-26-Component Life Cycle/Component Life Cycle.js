bef827d56bfcf8a26a5d549fe4a6c6d1a955bd41
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-13 20:58:43 +0530

    Create Component Life Cycle.js

---
+++ ALL REACT NOTES/Day-39-02-07-26-Component Life Cycle/Component Life Cycle.js
@@ -0,0 +1,29 @@
+Component Life Cycle
+Sudhakar Sharma (Sudhakar Sharma Naresh IT)
+•
+Jul 2
+React Hooks
+
+  - Hook is a service. 
+
+  - Service is a predefined business logic. 
+
+  - Service uses a single ton pattern. 
+
+  - It allows to customize and implement according to requirements. 
+
+  - It is a set of factories.
+
+  - Factory is a set of values and functions. 
+
+
+
+           values / functions => factory => service => components => app
+
+
+
+  - Technically hook is a function. 
+
+  - You can build custom hooks.
+
+ 
