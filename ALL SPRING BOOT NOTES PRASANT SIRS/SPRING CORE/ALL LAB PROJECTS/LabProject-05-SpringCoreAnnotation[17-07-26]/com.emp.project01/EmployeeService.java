d401310027eeae7d23f5e40614a8db227615f4c9
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-17 12:42:22 +0530

    Create EmployeeService.java

---
+++ ALL SPRING BOOT NOTES PRASANT SIRS/SPRING CORE/ALL LAB PROJECTS/LabProject-05-SpringCoreAnnotation[17-07-26]/com.emp.project01/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.emp.project01;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeService {
+
+    public void displayEmployeeDetails() {
+
+        System.out.println("Employee details loaded successfully.");
+
+    }
+
+}
