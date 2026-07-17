70f8b3c90dab4d424c892a84328cfe413b2e6f37
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-17 12:44:04 +0530

    Create NotificationService .java

---
+++ ALL SPRING BOOT NOTES PRASANT SIRS/SPRING CORE/ALL LAB PROJECTS/LabProject-05-SpringCoreAnnotation[17-07-26]/com.notification.project02/NotificationService .java
@@ -0,0 +1,16 @@
+package com.notification.project02;
+
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationService {
+
+    public void sendNotification() {
+
+        System.out.println("Notification sent successfully.");
+
+    }
+
+}
