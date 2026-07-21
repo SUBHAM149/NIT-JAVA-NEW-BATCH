41457e2338b0b32c6253d9281a239e1716bd92ed
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-21 21:14:46 +0530

    Create AppConfig.java

---
+++ ALL SPRING BOOT NOTES PRASANT SIRS/SPRING CORE/ALL LAB PROJECTS/LabProject-06-SpringAnnotation[20-07-26]/com.sub.parking/AppConfig.java
@@ -0,0 +1,10 @@
+package com.sub.parking;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.sub.parking")
+public class AppConfig {
+
+}
