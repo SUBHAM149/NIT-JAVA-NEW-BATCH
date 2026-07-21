ad99ba193758b1b270ae0d4d635b9566ca39f4b4
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-21 21:12:34 +0530

    Create AppConfig.java

---
+++ ALL SPRING BOOT NOTES PRASANT SIRS/SPRING CORE/ALL LAB PROJECTS/LabProject-06-SpringAnnotation[20-07-26]/com.nit.banking2/AppConfig.java
@@ -0,0 +1,10 @@
+package com.nit.banking2;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.nit.banking2")
+public class AppConfig {
+
+}
