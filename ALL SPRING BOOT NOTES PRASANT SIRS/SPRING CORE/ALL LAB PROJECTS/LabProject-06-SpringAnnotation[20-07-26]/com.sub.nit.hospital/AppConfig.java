856f25046ad942ea4f624fb2fb4776e41c538e59
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-22 22:35:03 +0530

    Create AppConfig.java

---
+++ ALL SPRING BOOT NOTES PRASANT SIRS/SPRING CORE/ALL LAB PROJECTS/LabProject-06-SpringAnnotation[20-07-26]/com.sub.nit.hospital/AppConfig.java
@@ -0,0 +1,10 @@
+package com.sub.nit.hospital;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.sub.nit.hospital")
+public class AppConfig {
+
+}
