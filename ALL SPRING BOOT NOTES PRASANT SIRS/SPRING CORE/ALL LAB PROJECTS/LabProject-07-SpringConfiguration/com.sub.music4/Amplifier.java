073fc5a8ece35e500e9bc1808285d898eafa63fb
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-27 12:00:51 +0530

    Create Amplifier.java

---
+++ ALL SPRING BOOT NOTES PRASANT SIRS/SPRING CORE/ALL LAB PROJECTS/LabProject-07-SpringConfiguration/com.sub.music4/Amplifier.java
@@ -0,0 +1,12 @@
+package com.sub.music4;
+
+public class Amplifier {
+
+    public Amplifier() {
+        System.out.println("Amplifier Constructor");
+    }
+
+    public void boostSignal() {
+        System.out.println("Signal boosted by amplifier");
+    }
+}
