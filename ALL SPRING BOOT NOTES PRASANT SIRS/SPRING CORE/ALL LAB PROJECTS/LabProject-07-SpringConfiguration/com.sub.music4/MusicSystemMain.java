c718a491774c31534677a6f2f288402a2bad1917
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-27 12:01:45 +0530

    Create MusicSystemMain.java

---
+++ ALL SPRING BOOT NOTES PRASANT SIRS/SPRING CORE/ALL LAB PROJECTS/LabProject-07-SpringConfiguration/com.sub.music4/MusicSystemMain.java
@@ -0,0 +1,16 @@
+package com.sub.music4;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MusicSystemMain {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new AnnotationConfigApplicationContext(MusicConfig.class);
+
+		MusicPlayer player = context.getBean(MusicPlayer.class);
+
+		player.playMusic();
+	}
+}
