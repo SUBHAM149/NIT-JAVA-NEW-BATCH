c0b7631981229d6d65b695a82a9957933540c276
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-28 12:08:46 +0530

    Create PaymentService.java

---
+++ ALL SPRING BOOT NOTES PRASANT SIRS/SPRING CORE/ALL LAB PROJECTS/LabProject-07-SpringConfiguration/com.shopping8/PaymentService.java
@@ -0,0 +1,14 @@
+package com.shopping8;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentService {
+
+    public void makePayment() {
+
+        System.out.println("Payment Completed Successfully.");
+
+    }
+
+}
