55e53c407a9156a3a7eea764486d6e302d8e71a7
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-28 12:06:07 +0530

    Add AppConfig class for Spring configuration

---
+++ ALL SPRING BOOT NOTES PRASANT SIRS/SPRING CORE/ALL LAB PROJECTS/LabProject-07-SpringConfiguration/com.hospital6/AppConfig.java
@@ -0,0 +1,10 @@
+package com.hospital6;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.hospital6")
+public class AppConfig {
+
+}
