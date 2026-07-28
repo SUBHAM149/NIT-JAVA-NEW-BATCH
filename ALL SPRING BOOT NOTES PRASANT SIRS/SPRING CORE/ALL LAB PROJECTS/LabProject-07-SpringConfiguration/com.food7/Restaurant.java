e039fbcf15237d61e8f2638f8321f4596f6d506d
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-28 12:07:08 +0530

    Create Restaurant.java

---
+++ ALL SPRING BOOT NOTES PRASANT SIRS/SPRING CORE/ALL LAB PROJECTS/LabProject-07-SpringConfiguration/com.food7/Restaurant.java
@@ -0,0 +1,13 @@
+package com.food7;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Restaurant {
+
+    public void prepareFood() {
+
+        System.out.println("Restaurant prepared Veg Biryani.");
+    }
+
+}
