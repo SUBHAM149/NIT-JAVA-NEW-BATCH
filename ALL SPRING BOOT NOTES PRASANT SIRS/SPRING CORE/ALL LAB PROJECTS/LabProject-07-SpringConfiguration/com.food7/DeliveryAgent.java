1ed26f6a3bb203ffb8b25222abe4455577de456f
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-28 12:07:51 +0530

    Add DeliveryAgent class for order delivery functionality

---
+++ ALL SPRING BOOT NOTES PRASANT SIRS/SPRING CORE/ALL LAB PROJECTS/LabProject-07-SpringConfiguration/com.food7/DeliveryAgent.java
@@ -0,0 +1,21 @@
+package com.food7;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeliveryAgent {
+
+    @Autowired
+    private Restaurant restaurant;
+
+    public void deliverOrder() {
+
+        System.out.println("Delivery Agent received the order.");
+
+        restaurant.prepareFood();
+
+        System.out.println("Order Delivered Successfully.");
+    }
+
+}
