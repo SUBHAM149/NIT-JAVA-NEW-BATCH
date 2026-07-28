c932653f69bbdfbe0c2dcf07a25444169dbf265b
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-28 12:09:30 +0530

    Add AppConfig class for Spring configuration

---
+++ ALL SPRING BOOT NOTES PRASANT SIRS/SPRING CORE/ALL LAB PROJECTS/LabProject-07-SpringConfiguration/com.shopping8/AppConfig.java
@@ -0,0 +1,10 @@
+package com.shopping8;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.shopping8")
+public class AppConfig {
+
+}
