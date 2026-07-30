5bcf4e8e6e89a17fe50287182251d12d0cdeabf4
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-30 21:43:39 +0530

    Create SpringBootProject13MangoDbApplication.java

---
+++ All SPRING BOOT NOTES/Day-58-30-07-26-Validation AND MongoDB/SpringBoot_Project_13-MangoDB/com.sub.nit/SpringBootProject13MangoDbApplication.java
@@ -0,0 +1,14 @@
+package com.sub.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject13MangoDbApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootProject13MangoDbApplication.class, args);
+    }
+
+}
