c402ea1427fafdee962ada2829da5a628dfe2c7e
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-07-31 21:49:41 +0530

    Delete All SPRING BOOT NOTES/Day-59-31-07-26-Exception Handling & DataJPA Testing/SpringBoot_Project_12-Validation directory

--- All SPRING BOOT NOTES/Day-59-31-07-26-Exception Handling & DataJPA Testing/SpringBoot_Project_12-Validation/com.sub.nit.repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sub.nit.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.sub.nit.entity.Student;
-@Repository
-public interface StudentRepository extends JpaRepository<Student,Integer>{
-
-}
