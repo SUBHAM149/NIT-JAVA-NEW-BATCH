bf9c0317f4af427bcaec78002dbdfb2ecde32770
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-08-10 20:57:54 +0530

    Create SpringBootProject22JwtAuthenticationApplication.java

---
+++ All SPRING BOOT NOTES/Day-66-08-08-26-JWT Authentication/SpringBoot_Project_22-JWT-Authentication/com.example.jwtproject/SpringBootProject22JwtAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject22JwtAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject22JwtAuthenticationApplication.class, args);
+	}
+
+}
