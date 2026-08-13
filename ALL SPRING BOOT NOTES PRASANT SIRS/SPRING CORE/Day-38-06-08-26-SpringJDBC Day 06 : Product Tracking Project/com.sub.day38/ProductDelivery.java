fc80b39bd813d1c4e5897ed40f646da5ecf1e8b9
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-08-13 19:51:05 +0530

    Create ProductDelivery.java

---
+++ ALL SPRING BOOT NOTES PRASANT SIRS/SPRING CORE/Day-38-06-08-26-SpringJDBC Day 06 : Product Tracking Project/com.sub.day38/ProductDelivery.java
@@ -0,0 +1,17 @@
+package com.sub.day38;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class ProductDelivery {
+
+    private String productName;
+    private String productShippingAddress;
+    private float productPrice;
+    private int productQuantity;
+    private Date productPlaceDate;
+    
+    
+}
