7178fce337eff0d1094257cf01b5e310984b038f
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-08-28 09:45:17 +0530

    Create SpringBootProject30SecurityApplication.java

---
+++ All SPRING BOOT NOTES/Day-79-27-08-26-Scecurity Project-15 And Spring Boot Project-30/SpringBoot_Project_30-Security/com.example/SpringBootProject30SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject30SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject30SecurityApplication.class, args);
+	}
+
+}
