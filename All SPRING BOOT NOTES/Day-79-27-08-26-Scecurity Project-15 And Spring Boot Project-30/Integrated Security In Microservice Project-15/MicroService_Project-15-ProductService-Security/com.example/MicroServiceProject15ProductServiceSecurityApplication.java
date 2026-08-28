ba5df8c9cb5838efcbbf5669cae06cc76db2dab8
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-08-28 10:00:48 +0530

    Create MicroServiceProject15ProductServiceSecurityApplication.java

---
+++ All SPRING BOOT NOTES/Day-79-27-08-26-Scecurity Project-15 And Spring Boot Project-30/Integrated Security In Microservice Project-15/MicroService_Project-15-ProductService-Security/com.example/MicroServiceProject15ProductServiceSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceProject15ProductServiceSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceProject15ProductServiceSecurityApplication.class, args);
+	}
+
+}
